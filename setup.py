34cbe0ea00709681bae345a4be5829808d473336
Fatih Keles <fatihkeles@gmail.com>
2019-12-03 18:04:14 +0200

    python egg

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 18:49:43 2019
+
+@author: fakeles
+""" 
+
+""" $ python setup.py bdist_egg """
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tcmb',
+    version='0.1.0',
+    description='A data scraper for importing XML files from tcmb.gov.tr',
+    long_description='I would just open("README.md").read() here',
+    author='Fatih Keles',
+    author_email='fatihkeles@gmail.com',
+    url='https://github.com/fatih-keles/scrapy-tcmb',
+    packages=find_packages(exclude=['*tests*']),
+)
